16073e1822c5c755a123a3677024f3f60f9738f8
pranaykumaradepu <pranaykumarpc96@gmail.com>
2024-06-19 23:43:24 +0530

    add Js first note

---
+++ Day-14-Js/script.js
@@ -0,0 +1,17 @@
+myName = 'pranayKumarAdepu'
+console.log('length of my name is : ',myName.length)
+
+firstName ='pranayKumar'  //q2 : 
+console.log('first letter of myName is ',firstName[0])
+
+console.log('last letter of myName is ',firstName[firstName-1])
+
+console.log(''.length, 'length of empty string')
+
+console.log(' '.length, 'length of single space string')
+
+
+
+
+
+
